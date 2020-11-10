d571089de3d7bea9d79f19cfed4d24842bfa6d6e
Evelin <szabadosevelin7@gmail.com>
2020-11-10 12:18:28 +0100

    Send new message sub if user is included

--- src/resolvers/Mutations/MessageActions.ts
+++ src/resolvers/Mutations/MessageActions.ts
@@ -1,6 +1,9 @@
-import { GraphQLResolveFn } from '../types'
+import { GraphQLResolveFn, User } from '../types'
+import { getUserId } from '../../utils';
+
 
 const newMessage: GraphQLResolveFn = async (parent, args, context, info) => {
+    const userId = getUserId(context.request)
     const newMessage = await context.db.messages.create({
         data: {
             sender: {
@@ -12,7 +15,12 @@ const newMessage: GraphQLResolveFn = async (parent, args, context, info) => {
             text: args.text
         }
     })
-    context.pubsub.publish("NEW_MESSAGE", newMessage)
+    let chatUsers = await context.db.chats.findOne({ where: { id: parseInt(args.chatId) } }).users()
+
+    if (chatUsers.filter((user: User) => user.id === Object.values(userId)[0]).length > 0) {
+        context.pubsub.publish("NEW_MESSAGE", newMessage)
+    }
+
     return newMessage;
 }
 export default newMessage;
--- src/resolvers/types.ts
+++ src/resolvers/types.ts
@@ -22,3 +22,48 @@ export type GraphQLSubscriptionFn = {
     subscribe: GraphQLResolveFn,
     resolve: (payload: any) => any
 }
+
+export type Chat = {
+    id: number
+    lastUpdated: String
+    users: [User]
+    messages: [Message]
+}
+export type Message = {
+    id: number
+    sender: User
+    text: String
+}
+export type User = {
+    id: number
+    displayName: String
+    email: String
+    password: String
+    profilePictureUrl: String
+    status: Status
+    friends: [FriendShip]
+    friendRequestsSent: [FriendRequest],
+    friendRequestsRecieved: [FriendRequest]
+}
+
+export type FriendRequest = {
+    id: number,
+    sender: User
+    reciever: User
+}
+
+export type FriendShip = {
+    id: number
+    users: [User]
+}
+
+export type AuthPayload = {
+    token: String
+    user: User
+}
+
+export enum Status {
+    OFFLINE,
+    BUSY,
+    AVAILABLE,
+}
