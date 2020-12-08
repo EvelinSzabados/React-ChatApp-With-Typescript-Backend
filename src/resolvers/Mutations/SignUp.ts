1b85a791c81494322270f9b82bc10b103688a3b1
Evelin <szabadosevelin7@gmail.com>
2020-12-08 10:21:02 +0100

    Fix typing in signup mutation

--- src/resolvers/Mutations/SignUp.ts
+++ src/resolvers/Mutations/SignUp.ts
@@ -1,4 +1,4 @@
-import { GraphQLResolveFn } from '../../common/types'
+import { GraphQLResolveFn, Status } from '../../common/types'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { APP_SECRET } from '../../common/utils'
@@ -11,7 +11,7 @@ const signup: GraphQLResolveFn = async (parent, args, context, info) => {
             email: args.email,
             password: password,
             displayName: args.displayName,
-            status: 'OFFLINE',
+            status: Status.AVAILABLE,
             profilePictureUrl: '',
             friends: [],
             friendRequestsSent: [],
@@ -21,6 +21,15 @@ const signup: GraphQLResolveFn = async (parent, args, context, info) => {
     })
 
     const token = jwt.sign({ userId: user.id }, APP_SECRET, { noTimestamp: true })
+
+    const options = {
+        maxAge: 2 * 3600 * 1000 + 2000, //expires in a day
+        httpOnly: true,
+        domain: "localhost",
+
+    }
+    context.response.cookie('Bearer', token, options)
+
     return {
         token,
         user,
